44a73fb30428b1215a3ec640bb4b3ede4721b54b
Oussama-CHAIBI <wesschaibi2017@gmail.com>
2022-12-24 00:54:14 +0100

    hjh

--- scripts/fund_and_withdrow.py
+++ scripts/fund_and_withdrow.py
@@ -15,7 +15,7 @@ def fund():
 def withdrow():
     fund_me = FundMe[-1]
     account = get_account()
-    fund_me.withdrow({"from": account})
+    fund_me.withdrow({"froms": account})
 
 
 def main():
